34be1d7a1a5f56ff2a73dfc3ed6c14622760bece
Ruslan Garapov <ruslangarapov1994@gmail.com>
2022-10-01 17:40:56 +0300

    catalog changes

--- page.php
+++ page.php
@@ -14,7 +14,39 @@
 
 get_header();
 ?>
-
+	<div class="breadcrums center_block">
+        <div class="breadcrums__item">
+            <div class="breadcrums__in">
+                <a href="/">Главная</a>
+                <div class="breadcrums__splash"></div>
+                <a href="#">Подкатегория</a>
+                <div class="breadcrums__splash"></div>
+                <span>Данная страница</span>
+            </div>
+        </div>
+    </div>
+	<div class="page_top center_block">
+		<header class="entry-header">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		</header><!-- .entry-header -->
+		<p class="woocommerce-result-count">
+			<?php
+				// phpcs:disable WordPress.Security
+				if ( 1 === intval( $total ) ) {
+					_e( 'Showing the single result', 'woocommerce' );
+				} elseif ( $total <= $per_page || -1 === $per_page ) {
+					/* translators: %d: total results */
+					printf( _n( 'Showing all %d result', 'Showing all %d results', $total, 'woocommerce' ), $total );
+				} else {
+					$first = ( $per_page * $current ) - $per_page + 1;
+					$last  = min( $total, $per_page * $current );
+					/* translators: 1: first result 2: last result 3: total results */
+					printf( _nx( 'Showing %1$d&ndash;%2$d of %3$d result', 'Showing %1$d&ndash;%2$d of %3$d results', $total, 'with first and last result', 'woocommerce' ), $first, $last, $total );
+				}
+			// phpcs:enable WordPress.Security
+			?>
+		</p>
+	</div>
 	<main id="primary" class="content">
 		<div class="inner_page center_block">
 			<? if( is_product_category() || is_shop() ) { ?>
@@ -37,7 +69,7 @@ get_header();
 				endwhile; // End of the loop.
 				?>
 			</div>
-	</div>
+		</div>
 	</main><!-- #main -->
 
 <?php
