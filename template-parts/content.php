a1619afe0d3e7502a1c1d6cd198b0ee09dcc3034
Ruslan Garapov <ruslangarapov1994@gmail.com>
2022-10-03 03:21:19 +0300

    product page changes

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,25 +10,38 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
-		
+	<div class="breadcrums center_block">
+        <div class="breadcrums__item">
+            <div class="breadcrums__in">
+                <a href="/">Главная</a>
+                <div class="breadcrums__splash"></div>
+                <a href="#">Подкатегория</a>
+                <div class="breadcrums__splash"></div>
+                <span>Данная страница</span>
+            </div>
+        </div>
+    </div>
+	<? if (!is_product()): ?>
+		<header class="entry-header">
+			<?php
+			if ( is_singular() ) :
+				the_title( '<h1 class="entry-title">', '</h1>' );
+			else :
+				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			endif;
+			
 
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				shumof_posted_on();
-				shumof_posted_by();
+			if ( 'post' === get_post_type() ) :
 				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+				<div class="entry-meta">
+					<?php
+					shumof_posted_on();
+					shumof_posted_by();
+					?>
+				</div><!-- .entry-meta -->
+			<?php endif; ?>
+		</header><!-- .entry-header -->
+	<?php endif; ?>
 
 	<?php shumof_post_thumbnail(); ?>
 
