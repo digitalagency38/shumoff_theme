e8ca6643f46f9dc6d11be9ab4514dc1deb1bd708
Ruslan Garapov <ruslangarapov1994@gmail.com>
2022-10-10 05:35:28 +0300

    ghj

--- functions.php
+++ functions.php
@@ -337,8 +337,5 @@ function maximum_api_filter($query_params) {
 	$query_params['per_page']['maximum'] = 10000;
 	$query_params['per_page']['default'] = 500;
 	return $query_params;
- }
- add_filter('rest_product_collection_params', 'maximum_api_filter', 10, 1 );
-ams;
- }
- add_filter('rest_product_collection_params', 'maximum_api_filter', 10, 1 );
+}
+add_filter('rest_product_collection_params', 'maximum_api_filter', 10, 1 );
