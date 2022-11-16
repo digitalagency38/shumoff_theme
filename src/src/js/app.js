c8eeadf4fb76d9996737f729089dc40a3b0f64ce
Ruslan Garapov <ruslangarapov1994@gmail.com>
2022-11-16 05:59:22 +0300

    calc fixed

--- src/src/js/app.js
+++ src/src/js/app.js
@@ -25,25 +25,36 @@ import ExpBlock from '../blocks/modules/exp_block/exp_block.js';
 import ServBlock from '../blocks/modules/page_service/page_service.js';
 import ProductBlock from '../blocks/modules/product_block/product_block.js';
 import FixedBlock from '../blocks/modules/fixed_panel/fixed_panel.js';
-import Calculator from '../blocks/modules/calc/calc.js';
+// import Calculator from '../blocks/modules/calc/calc.js';
 
 
 
 window.app = new Vue({
     el: '#app',
     data: () => ({
+        message: 'Calculator inited',
+        models: [],
+        url: '',
+        wooClientKey: 'ck_a942f49a79ed49679c1688d561f4db349b5487dd',
+        wooClientSecret: 'cs_cdc1f129e2de70b47b1f196f785820632210e30a',
+        authHeader: '',
+        perPage: 1000,
+        products: [],
+        areas: [],
         brands: [],
         selectedBrand: "",
         selectedModel: "",
+        selectedBody: "",
         uniqueModels: [],
-        get: loadPortfolioGetParams(),
+        effect: "max",
+        nonce: null,
+        isMounted: false,
         isLoaded: false,
         sizes: {
             tablet: 1024,
             mobile: 768,
             window: window.innerWidth
         },
-        isLoaded: false,
         footerBlock: new FooterBlock(),
         textBlock: new TextBlock(),
         mapBlock: new MapBlock(),
@@ -58,7 +69,8 @@ window.app = new Vue({
         servBlock: new ServBlock(),
         productBlock: new ProductBlock(),
         fixedBlock: new FixedBlock(),
-        calculator: new Calculator(),
+        get: loadPortfolioGetParams()
+        // calculator: new Calculator(),
     }),
     watch: {
         'firstBlock.index'(newValue) {
@@ -70,7 +82,10 @@ window.app = new Vue({
         },
         selectedBrand: function(newValue, oldValue) {
             if (newValue === oldValue) return;
-            this.selectedModel = this.get.type;
+            if (this.selectedModel !== "Все") {
+                this.selectedModel = "";
+            }
+            this.selectedModel = this.get.type ? this.get.type : this.selectedModel;
             const selectedModels = this.models.filter(model => model.brand === newValue);
 
 
@@ -108,7 +123,7 @@ window.app = new Vue({
             
         });
         this.isLoaded = true;
-        setTimeout(() => {
+        setTimeout(async () => {
             this.headerBlock.init();
             this.mapBlock.init();
             this.footerBlock.init();
@@ -123,7 +138,7 @@ window.app = new Vue({
             this.servBlock.init();
             this.productBlock.init();
             this.fixedBlock.init();
-            this.calculator.init();
+            // this.calculator.init();
 
             const allSelects = document.querySelectorAll("select");
             allSelects.forEach(function (el) {
@@ -133,14 +148,25 @@ window.app = new Vue({
                 });
             });
 
+            this.setApiParams();
+            await this.getProducts().then(result => {
+                this.products = result;
+                
+            });
             this.models = loadModels();
             this.filterBrands();
-            console.log(this.get);
+            this.getNonce();
+            this.isMounted = true;
         }, 0);
             
         
     },
     methods: {
+        setEffect(effect) {
+            if (effect === this.effect) return;
+            this.effect = effect;
+            this.reloadAreas(this.effect);
+        },
         filterBrands() {
             let uniqueBrands = [];
             this.models.map(model => {
@@ -154,7 +180,145 @@ window.app = new Vue({
                 return model;
             })
             console.log(uniqueBrands);
-            this.selectedBrand = this.get.vendor;
+            this.selectedBrand = 'Все';
+            this.selectedModel = 'Все';
+        },
+        setModels(models) {
+            this.models = models;
+        },
+        basicAuth(key, secret) {
+            let hash = btoa(key + ':' + secret);
+            return "Basic " + hash;
+        },
+        setApiParams() {
+            if (document.querySelector('[data-site-url]')) {
+                this.url = document.querySelector('[data-site-url]').dataset.siteUrl;
+            }
+            this.authHeader = this.basicAuth(this.wooClientKey, this.wooClientSecret);
+            console.log(this.url, this.authHeader);
+        },
+        getProductsThatHasArea(products) {
+            let filteredProducts = [];
+            products.map(product => {
+                if ('attributes' in product) {
+                    product.attributes.map(attribute => {
+                        if (attribute.name === 'Область применения') {
+                            filteredProducts.push(product);
+                        };
+                        return attribute;
+                    })
+                };
+                return product;
+            });
+            return filteredProducts;
+        },
+        createAreasArray(filterProducts) {
+            this.areas = [];
+            let areaNames = [];
+            filterProducts.map(product => {
+                if ('attributes' in product) {
+                    product.attributes.map(attribute => {
+                        if (attribute.name === 'Область применения') {
+                            attribute.options.map(option => {
+                                if (!areaNames.includes(option)) {
+                                    areaNames.push(option);
+                                    this.areas = [
+                                        ...this.areas,
+                                        {
+                                            name: option,
+                                            isSelected: true,
+                                            isOpened: false,
+                                            weight: +product.weight,
+                                            price: product.sale_price !== "" ? +product.sale_price : +product.price,
+                                            products: [
+                                                product
+                                            ]
+                                        }
+                                    ]
+                                } else {
+                                    this.areas.map(item => {
+                                        console.log('item', item.name, option);
+                                        if (item.name === option) {
+                                            item.weight += +product.weight;
+                                            item.price += product.sale_price !== "" ? +product.sale_price : +product.price;
+                                            item.products.push(product);
+                                        }
+                                        return item;
+                                    })
+                                }
+                            });
+                        };
+                        return attribute;
+                    })
+                };
+                return product;
+            });
+        },
+        async splitProductsByArea(products) {
+            let filteredProducts = await this.getProductsThatHasArea(products);
+            this.createAreasArray(filteredProducts);
+        },
+        async getProducts() {        
+            try {
+                const response = await fetch(`${this.url}/wp-json/wc/v3/products?per_page=${this.perPage}`, {
+                    headers: {"Authorization": this.authHeader}
+                });
+                const products = response.json();
+                return products;
+            }
+            catch (error) {
+                console.log(error);
+            }
+        },
+        reloadAreas(effect) {
+            let prods = [];
+            this.products.filter(product => {
+                product.attributes.map(attr => {
+                    if (attr.name === "Эффект" && JSON.stringify(attr.options).includes(this.effect)) {
+                        console.log('123123123123')
+                        prods.push(product);
+                    }
+                    return attr;
+                })
+            });
+            this.splitProductsByArea(prods);
+        },
+        getNonce() {
+            if (!document.querySelector("[data-nonce]")) return;
+            this.nonce = document.querySelector("[data-nonce]").dataset.nonce;
+            console.log(this.nonce);
+        },
+        addToCart() {
+            console.log(this.areas);
+            this.areas.map(area => {
+                area.products.map(product => {
+                    console.log(product);
+                    $.ajax({
+                        type: 'POST',
+                        url: `${this.url}/wp-json/wc/store/cart/add-item`,
+                        dataType: 'json',
+                        headers: {
+                          'X-WC-Store-API-Nonce': this.nonce
+                        },
+                        data: {
+                          id : product.id,
+                          quantity: 1,
+                          variation: [
+                            {
+                                attribute: "size",
+                                value: '1 шт',
+                            }
+                          ],
+                        },
+                        success: function(result) {
+                            console.log(result);
+                        }
+                    });
+                    return product;
+                });
+                return area;
+            })
+           
         },
         clearFilter() {
             this.get.vendor = "Все";
@@ -169,6 +333,54 @@ window.app = new Vue({
         },
         isTablet: function () {
             return this.sizes.window < this.sizes.tablet && this.sizes.window > this.sizes.mobile;
+        },
+        selectedProducts() {
+            let products = [];
+            this.products.map(product => {
+                if ('attributes' in product) {
+                    product.attributes.map(attribute => {
+                        if (attribute.name === 'Тип кузова') {
+                            attribute.options.map(opt => {
+                                console.log(opt, this.selectedBody)
+                                if (opt === this.selectedBody) {
+                                    products.push(product);
+                                }
+                            })
+                        };
+                        return attribute;
+                    })
+                };
+                return product;
+            });
+            console.log('products', products);
+            return products;
+        },
+        selectedAreas() {
+            return this.areas.filter(area => area.isSelected === true);
+        },
+        availableBodies() {
+            let bodies = []
+            let selectedModel = this.uniqueModels.filter(model => model.name === this.selectedModel);
+            selectedModel.map(model => {
+                bodies = [
+                    ...model.bodies
+                ];
+                return model;
+            })
+
+            return bodies;
+        },
+        total() {
+            let total = {
+                price: 0,
+                weight: 0
+            }
+            this.selectedAreas.map(area => {
+                total.price += +area.price;
+                total.weight += +area.weight;
+                return area;
+            });
+            return total;
         }
     },
 });
@@ -180,6 +392,9 @@ $(function () {
     
     $('.flex-control-thumbs').wrapAll('<div class="flex_thumb_item">');   
     $('#order_review_heading, #order_review, .cart_totals').wrapAll('<div class="block_right_status"><div class="block_right_status--item">');
+    $('.block_right_status').prepend('<div class="sticky_top"></div>');
+    $('.block_right_status').append('<div class="sticky_bottom"></div>');
+    
     if (document.querySelector('.form-row.place-order')) {
         $('.block_right_status--item').append('<div class="here_will_be_button"></div>')
         $('.form-row.place-order').appendTo('.here_will_be_button');
